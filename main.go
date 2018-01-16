9230661e1c4f8c1925b05c352b9d7c55e84fc3f1
dudulu <wack1001@163.com>
2018-01-16 23:47:15 +0800

    change

--- main.go
+++ main.go
@@ -5,7 +5,22 @@ import (
 	"github.com/astaxie/beego"
 	"github.com/lattecake/blog/models"
 	"github.com/lattecake/blog/helper"
+	"github.com/astaxie/beego/logs"
+	"runtime"
 )
+
+func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	var logFileName = beego.AppConfig.String("logs_path") + "blog.lattecake.com.log"
+	beego.SetLogger(logs.AdapterFile, `{"filename": "`+logFileName+`","daily":true,"perm":"0644", "maxdays":365, "rotateperm": "0644"}`)
+	logs.EnableFuncCallDepth(true)
+
+	if beego.AppConfig.String("runmode") != "dev" {
+		beego.BeeLogger.DelLogger("console")
+		logs.Async()
+	}
+}
 
 func main() {
 
